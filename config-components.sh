301091e613925bc5760ce95a9723ba12c489a43e
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-09-07 16:12:08 -0300

    Update

--- config-components.sh
+++ config-components.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Create
-./Gust/scripts/create.project--br.________.sh
-./Gust/scripts/create.src.1.0.0.0.sh
+#./Gust/scripts/create.project--br.________.sh
+#./Gust/scripts/create.src.1.0.0.0.sh
 
 #Delete
-./Gust/scripts/delete.bin.obj.MyClass--br.________.sh
+#./Gust/scripts/delete.bin.obj.MyClass--br.________.sh
 
 #sn create/update
 ./Gust/scripts/create.sn--br._____.sh
