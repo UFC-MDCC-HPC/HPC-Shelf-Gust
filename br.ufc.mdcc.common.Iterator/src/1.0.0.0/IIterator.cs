cbc4444e503956afa2ee4428cce2a2bece9d8d1e
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-10-22 18:24:34 -0300

    Commons include

---
+++ br.ufc.mdcc.common.Iterator/src/1.0.0.0/IIterator.cs
@@ -0,0 +1,40 @@
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
+using System;
+using br.ufc.mdcc.hpcshelf.mapreduce.port.environment.PortTypeIterator;
+
+namespace br.ufc.mdcc.common.Iterator { 
+	
+	public interface IIterator<T> : IData, BaseIIterator<T>
+		where T:IData
+	{
+		IIteratorInstance<T> newIteratorInstance();
+		object createItem ();
+
+	} // end main interface 
+
+	public interface IIteratorInstance<T> : IDataInstance, ICloneable, IPortTypeIterator
+		where T:IData
+	{
+		// consumer:
+		ICloneable createItem ();
+
+		// - raises exception if has finished
+		void put(object item);
+
+		void putAll(IIteratorInstance<T> items);
+
+		// - raises exception if has finished and not restarted
+		void finish();
+
+		// producer:
+
+		// - raises exception if has finished
+		bool fetch_next (out object result);
+
+		bool has_next(); 
+	}
+
+
+
+} // end namespace 
