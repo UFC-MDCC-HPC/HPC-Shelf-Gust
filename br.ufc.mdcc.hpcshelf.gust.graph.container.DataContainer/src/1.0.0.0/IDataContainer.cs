40b406cf2b583b5bc4ed3e0a97b895f99ba50ac7
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-10-25 21:54:17 -0300

    Datacontainer

--- br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer/src/1.0.0.0/IDataContainer.cs
+++ br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer/src/1.0.0.0/IDataContainer.cs
@@ -1,12 +1,16 @@
+using System;
+using System.Collections.Generic;
 using br.ufc.pargo.hpe.kinds;
 using br.ufc.mdcc.hpcshelf.gust.graph.Vertex;
 using br.ufc.mdcc.hpcshelf.gust.graph.Edge;
 using br.ufc.mdcc.common.Data;
-using System.Collections.Generic;
-using System;
 
-namespace br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer {
-	public interface IDataContainer<V, E> : BaseIDataContainer<V, E>, IData where V:IVertex where E:IEdge<V> {
+namespace br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer 
+{
+	public interface IDataContainer<V, E> : BaseIDataContainer<V, E>, IData 
+	    where V:IVertex 
+	    where E:IEdge<V> 
+	{
 		
 	}
 
