97c83df054752fdb0888b47dc20b4dd9111ecb5f
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-12-14 03:44:22 -0300

    EdgeBasitToEdge VertexBasicToVertex

--- br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainerV/src/1.0.0.0/IDataContainerV2.cs
+++ br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainerV/src/1.0.0.0/IDataContainerV2.cs
@@ -1,6 +1,8 @@
 using br.ufc.pargo.hpe.kinds;
-using br.ufc.mdcc.hpcshelf.gust.graph.Vertex;
+using br.ufc.mdcc.hpcshelf.gust.graph.VertexBasic;
+using br.ufc.mdcc.hpcshelf.gust.graph.EdgeBasic;
 using br.ufc.mdcc.hpcshelf.gust.graph.Edge;
+//using br.ufc.mdcc.hpcshelf.gust.graph.Vertex;
 using br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer;
 using System;
 using System.Collections.Generic;
@@ -8,14 +10,14 @@ using System.Collections.Generic;
 namespace br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainerV
 {
 	public interface IDataContainerV<V, E> : BaseIDataContainerV<V, E>, IDataContainer<V, E>
-		where V:IVertex
-		where E:IEdge<V> {
+		where V:IVertexBasic
+		where E:IEdgeBasic<V> {
 		IDataContainerVInstance<V, E, int, IEdgeInstance<V, int>> DataContainerVInstance { get; set; }
 		IDataContainerVInstance<V, E, TV, TE> InstanceTFactory<TV, TE> (TE e) where TE: IEdgeInstance<V, TV>;
 	}
 	public interface IDataContainerVInstance<V, E, TV, TE> : IDataContainerInstance<V, E>, ICloneable 
-		where V:IVertex 
-		where E:IEdge<V> 
+		where V:IVertexBasic 
+		where E:IEdgeBasic<V> 
 		where TE: IEdgeInstance<V, TV> {
 
 		TV Vertex { get; set; }
