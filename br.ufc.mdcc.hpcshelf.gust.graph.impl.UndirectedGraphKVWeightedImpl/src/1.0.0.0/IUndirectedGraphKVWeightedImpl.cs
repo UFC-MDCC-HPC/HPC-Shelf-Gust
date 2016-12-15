7acb3d49fc122eb106a233e67cd6912b4cbc2304
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-12-15 18:50:21 -0300

    GraphComputationToData

--- br.ufc.mdcc.hpcshelf.gust.graph.impl.UndirectedGraphKVWeightedImpl/src/1.0.0.0/IUndirectedGraphKVWeightedImpl.cs
+++ br.ufc.mdcc.hpcshelf.gust.graph.impl.UndirectedGraphKVWeightedImpl/src/1.0.0.0/IUndirectedGraphKVWeightedImpl.cs
@@ -17,8 +17,16 @@ namespace br.ufc.mdcc.hpcshelf.gust.graph.impl.UndirectedGraphKVWeightedImpl
 where CTN:IDataContainerKV<V, E>
 where V:IVertex
 where E:IEdge<V> {
-		public override void main() {
+
+		public object newInstance () {
+			return newInstanceControl (0);
 		}
+
+		public object Instance {
+			get { return instanceControlT; }
+			set { this.instanceControlT = value; }
+		}
+
 		private object instanceControlT = null;
 		public object InstanceControlT { 
 			get{ 
@@ -54,6 +62,14 @@ where E:IEdge<V> {
 			public InstanceControlImpl(IGraphHelperKV<V, E, TV, TE> d){
 				delegator = d;
 			}
+
+			public object ObjValue {
+				get { return new Tuple<IGraphHelperKV<V, E, TV, TE>>(delegator); }
+				set { 
+					this.delegator = ((Tuple<IGraphHelperKV<V, E, TV, TE>>)value).Item1;
+				}
+			}
+
 			//************** implementation ***********************
 			public ICollection<TE> getAllEdges (TV sourceVertex, TV targetVertex) {
 				ICollection<TE> edges = null;
@@ -265,9 +281,14 @@ where E:IEdge<V> {
 			public bool isAllowingLoops () { return delegator.Container.AllowingLoops; }
 			public bool isAllowingMultipleEdges () { return delegator.Container.AllowingMultipleEdges; }
 
+			#region ICloneable implementation
 			public object Clone () {
-				throw new NotSupportedException ("Not Supported Exception");
+				IGraphHelperKV<V, E, TV, TE> d = (IGraphHelperKV<V, E, TV, TE>) this.delegator.Clone ();
+				InstanceControlImpl<V, E, TV, TE> clone = new InstanceControlImpl<V, E, TV, TE>(d);
+				return clone;
 			}
+			#endregion
+
 			public override string ToString () { 
 				IEnumerator<TV> vertexSet = this.vertexSet ().GetEnumerator();
 				IEnumerator<TE> edgeSet = this.edgeSet ();
@@ -356,6 +377,16 @@ where E:IEdge<V> {
 				get{ return this.container; }
 				set{ this.container = (IDataContainerKVInstance<V, E, TV, TE>)value; }
 			}
+
+			#region ICloneable implementation
+			public object Clone () {
+				IDataContainerKVInstance<V, E, TV, TE> c = (IDataContainerKVInstance<V, E, TV, TE>) this.Container.Clone ();
+				IGraphHelperKVImpl<V, E, TV, TE> clone = new IGraphHelperKVImpl<V, E, TV, TE> (c);
+				clone.count_edges = this.count_edges;
+				return clone;
+			}
+			#endregion
+
 			// ************************* implements ***********************************
 			public IEnumerator<TE> edgeSet () {
 				ICollection<TV> collection = container.DataSet.Keys;
