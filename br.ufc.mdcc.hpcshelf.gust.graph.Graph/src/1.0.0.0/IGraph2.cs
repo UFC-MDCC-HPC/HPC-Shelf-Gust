faac0092f45549a1a94c37e3692b6ab7e64fa021
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-12-15 01:35:51 -0300

    GraphComputationToData

--- br.ufc.mdcc.hpcshelf.gust.graph.Graph/src/1.0.0.0/IGraph2.cs
+++ br.ufc.mdcc.hpcshelf.gust.graph.Graph/src/1.0.0.0/IGraph2.cs
@@ -2,13 +2,12 @@ using br.ufc.pargo.hpe.kinds;
 using br.ufc.mdcc.hpcshelf.gust.graph.container.DataContainer;
 using br.ufc.mdcc.hpcshelf.gust.graph.Vertex;
 using br.ufc.mdcc.hpcshelf.gust.graph.Edge;
-using br.ufc.mdcc.common.Data;
 using System;
 using System.Collections.Generic;
 
 namespace br.ufc.mdcc.hpcshelf.gust.graph.Graph
 {
-	public interface IGraph<CTN, V, E> : BaseIGraph<CTN, V, E>, IData
+	public interface IGraph<CTN, V, E> : BaseIGraph<CTN, V, E>
 		where CTN:IDataContainer<V, E>
 		where V:IVertex
 		where E:IEdge<V>
