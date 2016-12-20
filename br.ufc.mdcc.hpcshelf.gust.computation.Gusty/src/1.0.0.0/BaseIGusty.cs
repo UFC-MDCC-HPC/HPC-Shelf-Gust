a66d5161f6f79faf24f0aac0c84ce7785bf03288
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2016-12-20 00:49:01 -0300

    Merge Properties of MapReduce and Gust

--- br.ufc.mdcc.hpcshelf.gust.computation.Gusty/src/1.0.0.0/BaseIGusty.cs
+++ br.ufc.mdcc.hpcshelf.gust.computation.Gusty/src/1.0.0.0/BaseIGusty.cs
@@ -1,23 +1,36 @@
 /* AUTOMATICALLY GENERATE CODE */
 
 using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Iterator;
 using br.ufc.mdcc.common.KVPair;
 using br.ufc.mdcc.common.Data;
-using br.ufc.mdcc.common.Iterator;
+using br.ufc.mdcc.common.Integer;
+using br.ufc.mdcc.hpcshelf.gust.binding.environment.BindingDirectMxNIterator;
+using br.ufc.mdcc.hpcshelf.gust.port.environment.PortTypeIterator;
 using br.ufc.mdcc.hpc.storm.binding.task.TaskBindingBase;
 using br.ufc.mdcc.hpcshelf.gust.port.task.TaskPortTypeAdvance;
-using br.ufc.mdcc.hpcshelf.gust.custom.GustyFunction;
+//using br.ufc.mdcc.hpcshelf.platform.Platform;
 using br.ufc.mdcc.hpcshelf.platform.Maintainer;
+using br.ufc.mdcc.hpcshelf.gust.custom.GustyFunction;
 
 namespace br.ufc.mdcc.hpcshelf.gust.computation.Gusty
 {
-	public interface BaseIGusty<M,OKey, OValue, TKey, TValue, RF> : IComputationKind 
+	public interface BaseIGusty<M, GF, TKey, TValue, OKey, OValue, PType, G> : IComputationKind 
 		where M:IMaintainer
-		where RF:IGustyFunction<OKey, OValue, TKey, TValue>
-		where OKey:IData
-		where OValue:IData
+		where GF:IGustyFunction<TKey, TValue, OKey, OValue, PType, G>
 		where TKey:IData
 		where TValue:IData
+		where OKey:IData
+		where OValue:IData
+		where PType:IData
+		where G:IData
 	{
+		IIterator<IKVPair<OKey, OValue>> Output {get;}
+		IKVPair<IInteger, PType> Partition_graph {get;}
+		G Graph {get;}
+		IBindingDirectMxNIterator<IPortTypeIterator> Feed_pairs {get;}
+		IBindingDirectMxNIterator<IPortTypeIterator> Collect_pairs {get;}
+		ITaskPort<ITaskPortTypeAdvance> Task_gusty {get;}
+		//IProcessingNode<M> Platform_gusty {get;}
 	}
 }
--- br.ufc.mdcc.hpcshelf.gust.computation.Gusty/src/1.0.0.0/IGusty2.cs
+++ br.ufc.mdcc.hpcshelf.gust.computation.Gusty/src/1.0.0.0/IGusty2.cs
@@ -1,19 +1,17 @@
 using br.ufc.pargo.hpe.kinds;
-using br.ufc.mdcc.hpcshelf.platform.Maintainer;
 using br.ufc.mdcc.hpcshelf.gust.custom.GustyFunction;
 using br.ufc.mdcc.common.Data;
+using br.ufc.mdcc.hpcshelf.platform.Maintainer;
 
 namespace br.ufc.mdcc.hpcshelf.gust.computation.Gusty
 {
-	public interface IGusty<M, GF, TKey, TValue, OKey, OValue, PType, G> : BaseIGusty<M, GF, TKey, TValue, OKey, OValue, PType, G>
+	public interface IGusty<M,TKey, TValue, OKey, OValue, RF> : BaseIGusty<M,TKey, TValue, OKey, OValue, RF>
 		where M:IMaintainer
-		where GF:IGustyFunction<TKey, TValue, OKey, OValue, PType, G>
+		where RF:IGustyFunction<TKey, TValue, OKey, OValue>
 		where TKey:IData
 		where TValue:IData
 		where OKey:IData
 		where OValue:IData
-		where PType:IData
-		where G:IData
 	{
 	}
 }
