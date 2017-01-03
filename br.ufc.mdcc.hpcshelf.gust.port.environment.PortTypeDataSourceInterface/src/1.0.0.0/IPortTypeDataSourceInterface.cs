5131bd0506b1124f89b6e35a3a54d4e852703ccb
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-03 00:54:08 -0300

    InputFormat fix

--- br.ufc.mdcc.hpcshelf.gust.port.environment.PortTypeDataSourceInterface/src/1.0.0.0/IPortTypeDataSourceInterface.cs
+++ br.ufc.mdcc.hpcshelf.gust.port.environment.PortTypeDataSourceInterface/src/1.0.0.0/IPortTypeDataSourceInterface.cs
@@ -13,20 +13,7 @@ namespace br.ufc.mdcc.hpcshelf.gust.port.environment.PortTypeDataSourceInterface
 	// IInputFormatInstance contém os dados do arquivo fonte, na forma int[0..|E|] Source, int[0..|E|] Target, onde |E| é o total de arestas i j em cada linha do arquivo.
 	// Executando IInputFormatInstance.extractBins() na instancia, os dados IInputFormatInstance.Source, IInputFormatInstance.Target 
 	// são particionados, sendo gerada a tabela de partes int[0 .. |V|] PartitionTABLE, onde |V| é o total de vértices.
-	// cada IInputFormatInstance.PartitionTABLE[VERTICEID] contém um inteiro de 0 até IInputFormatInstance.PART .
-	// IInputFormatInstance.extractBins() retorna um Dictionary<int, IInputFormatInstance>, sendo a chave a partiçao que pertecen o IInputFormatInstance.
+	// cada IInputFormatInstance.PartitionTABLE[VERTICEID] contém um inteiro de 0 até IInputFormatInstance.PARTITION_SIZE.
+	// IInputFormatInstance.extractBins() retorna um Dictionary<int, IInputFormatInstance>, onde chave (mesmo valor que IInputFormatInstance.PARTID) é a partiçao que pertece o IInputFormatInstance.
 	// É necessária a variável de ambiente PATH_GRAPH_FILE, apontando para o endereço do arquivo fonte de dados do grafo
-//	public class ShardInputFormat{
-//
-//		private int id;
-//		private IInputFormatInstance shard;
-//
-//		public IInputFormatInstance Shard { get{ return shard; } }
-//		public int Id { get{ return id; } set{ id = value; }}
-//
-//		public ShardInputFormat(int ID, IInputFormatInstance SHARD){
-//			this.id = ID;
-//			this.shard = SHARD;
-//		}
-//	}
 }
