eee2af861688911f2fc57fc1e41a41b106f92271
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-04 18:04:21 -0300

    PartitionFunction.PartitionTable

--- br.ufc.mdcc.hpcshelf.gust.impl.datasource.DataSourceImpl/src/1.0.0.0/IDataSourceImpl.cs
+++ br.ufc.mdcc.hpcshelf.gust.impl.datasource.DataSourceImpl/src/1.0.0.0/IDataSourceImpl.cs
@@ -13,20 +13,20 @@ namespace br.ufc.mdcc.hpcshelf.gust.impl.datasource.DataSourceImpl {
 	public class IDataSourceImpl<P> : BaseIDataSourceImpl<P>, IDataSource<P>
     where P:IMaintainer {
 		public override void main() {
-			IInputFormatInstance shardInstance = Data_format.newInstanceIF ();
-			Reader.Server = new DataSourceReader(shardInstance);
+			IInputFormatInstance input_format_instance = Data_format.newInstanceIF ();
+			Reader.Server = new DataSourceReader(input_format_instance);
 		}
 		private class DataSourceReader : IPortTypeDataSourceInterface {
 			
-			private IInputFormatInstance shardInstance;
+			private IInputFormatInstance input_format_instance;
 
 			public object fetchFileContentObject() {
-				return shardInstance;
+				return input_format_instance;
 				//string fileName = System.Environment.GetEnvironmentVariable (PATH_GRAPH_FILE);
 				//return File.ReadLines (fileName);
 			}
-			public DataSourceReader(IInputFormatInstance _shardInstance){
-				shardInstance = _shardInstance;
+			public DataSourceReader(IInputFormatInstance o){
+				input_format_instance = o;
 			}
 
 		}
