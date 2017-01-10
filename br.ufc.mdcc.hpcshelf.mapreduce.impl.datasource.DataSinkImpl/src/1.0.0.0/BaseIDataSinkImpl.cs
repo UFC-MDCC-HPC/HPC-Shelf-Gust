e042a44fb7308f1fceb2a2d7f9a4b07a17526b00
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-10 20:10:52 -0300

    OutputFormat

--- br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/BaseIDataSinkImpl.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/BaseIDataSinkImpl.cs
@@ -16,6 +16,27 @@ namespace br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl
 		where P:IMaintainer
 		where GOF:IOutputFormat
 	{
+		private GOF output_format = default(GOF);
+
+		protected GOF Output_format
+		{
+			get
+			{
+				if (this.output_format == null)
+					this.output_format = (GOF) Services.getPort("output_format");
+				return this.output_format;
+			}
+		}
+// GENERATED
+//		private IProcessingNode<M> platform_data_sink = null;
+//		public IProcessingNode<M> Platform_data_sink {
+//			get{
+//				if (this.platform_data_sink == null)
+//					this.platform_data_sink = (IProcessingNode<M>) Services.getPort("platform_data_sink");
+//				return this.platform_data_sink;
+//			}
+//		}
+		
 		private P platform = default(P);
 
 		protected P Platform
