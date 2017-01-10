cf23613a1e7250306e153a85028833231a6c8b43
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-10 17:57:15 -0300

    OutputFormat

--- br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/BaseIDataSinkImpl.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/BaseIDataSinkImpl.cs
@@ -8,11 +8,13 @@ using br.ufc.mdcc.hpc.storm.binding.environment.EnvironmentBindingBase;
 using br.ufc.mdcc.hpcshelf.mapreduce.port.environment.PortTypeDataSinkInterface;
 using br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSink;
 using br.ufc.mdcc.hpcshelf.platform.Maintainer;
+using br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat;
 
 namespace br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl 
 {
-	public abstract class BaseIDataSinkImpl<P>: Computation, BaseIDataSink<P>
+	public abstract class BaseIDataSinkImpl<P, GOF>: Computation, BaseIDataSink<P>
 		where P:IMaintainer
+		where GOF:IOutputFormat
 	{
 		private P platform = default(P);
 
--- br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/IDataSinkImpl.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl/src/1.0.0.0/IDataSinkImpl.cs
@@ -7,11 +7,13 @@ using System.Collections.Generic;
 using System.IO;
 using br.ufc.mdcc.hpcshelf.mapreduce.port.environment.PortTypeDataSinkInterface;
 using br.ufc.mdcc.hpcshelf.platform.Maintainer;
+using br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat;
           
 namespace br.ufc.mdcc.hpcshelf.mapreduce.impl.datasource.DataSinkImpl
 {
-	public class IDataSinkImpl<P> : BaseIDataSinkImpl<P>, IDataSink<P>
+	public class IDataSinkImpl<P, GOF> : BaseIDataSinkImpl<P>, IDataSink<P>
     where P:IMaintainer
+	where GOF:IOutputFormat
 	{
 		public override void main()
 		{
