305accbebad9c6b81441ef7216994cd9c334d306
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-10 00:59:45 -0300

    .

--- br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat/src/1.0.0.0/IOutputFormat.cs
+++ br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat/src/1.0.0.0/IOutputFormat.cs
@@ -1,5 +1,8 @@
+using System;
 using br.ufc.pargo.hpe.kinds;
 using br.ufc.mdcc.common.Data;
+using br.ufc.mdcc.common.Iterator;
+using br.ufc.mdcc.common.KVPair;
 
 namespace br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat
 {
@@ -7,5 +10,14 @@ namespace br.ufc.mdcc.hpcshelf.gust.graph.OutputFormat
 		where OKey:IData
 		where OValue:IData
 	{
+		IOutputFormatInstance<OKey,OValue> newInstance(IIteratorInstance<IKVPair<OKey,OValue>> iterator);
 	}
+
+	public interface IOutputFormatInstance<OKey,OValue> : IDataInstance, ICloneable
+		where OKey:IData
+		where OValue:IData
+	{
+		IIteratorInstance<IKVPair<OKey,OValue>> Iterator_instance { get; set; }
+	}
+
 }
