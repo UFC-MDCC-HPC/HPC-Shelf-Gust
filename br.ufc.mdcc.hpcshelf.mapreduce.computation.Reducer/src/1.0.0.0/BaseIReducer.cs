8837c4547f19121cc9d61deaf567f9000212cafc
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-10 04:30:56 -0300

    InputFormat

--- br.ufc.mdcc.hpcshelf.mapreduce.computation.Reducer/src/1.0.0.0/BaseIReducer.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.computation.Reducer/src/1.0.0.0/BaseIReducer.cs
@@ -4,6 +4,7 @@ using br.ufc.pargo.hpe.kinds;
 using br.ufc.mdcc.common.KVPair;
 using br.ufc.mdcc.common.Data;
 using br.ufc.mdcc.common.Iterator;
+using br.ufc.mdcc.hpcshelf.gust.graph.InputFormat;
 using br.ufc.mdcc.hpc.storm.binding.task.TaskBindingBase;
 using br.ufc.mdcc.hpcshelf.mapreduce.port.task.TaskPortTypeAdvance;
 using br.ufc.mdcc.hpcshelf.mapreduce.custom.ReduceFunction;
@@ -14,11 +15,11 @@ namespace br.ufc.mdcc.hpcshelf.mapreduce.computation.Reducer
 	public interface BaseIReducer<M, RF, GIF, TKey, TValue, OKey, OValue, G> : IComputationKind 
 		where M:IMaintainer
 		where RF:IReduceFunction<GIF, TKey, TValue, OKey, OValue, G>
-		where OKey:IData
-		where OValue:IData
+		where GIF:IInputFormat
 		where TKey:IData
 		where TValue:IData
-		where GIF:IData
+		where OKey:IData
+		where OValue:IData
 		where G:IData
 	{
 	}
