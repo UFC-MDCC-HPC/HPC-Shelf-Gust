29bbf536fd40cbfb8671487c8665bd834d6fdd34
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-10 01:14:04 -0300

    InputFormat

--- br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource/src/1.0.0.0/BaseIDataSource.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource/src/1.0.0.0/BaseIDataSource.cs
@@ -1,17 +1,18 @@
 /* AUTOMATICALLY GENERATE CODE */
 
 using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
 //using br.ufc.mdcc.hpc.storm.binding.environment.EnvironmentBindingBase;
 //using br.ufc.mdcc.hpcshelf.mapreduce.port.environment.PortTypeDataSourceInterface;
 //using br.ufc.mdcc.hpcshelf.platform.Platform;
 using br.ufc.mdcc.hpcshelf.platform.Maintainer;
-using br.ufc.mdcc.hpcshelf.gust.graph.InputFormat;
+//using br.ufc.mdcc.hpcshelf.gust.graph.InputFormat;
 
 namespace br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource
 {
 	public interface BaseIDataSource<P, GIF> : IComputationKind 
 		where P:IMaintainer
-		where GIF:IInputFormat
+		where GIF:IData //InputFormat
 	{
 		//IServerBase<IPortTypeDataSourceInterface> Reader {get;}
 		//IProcessingNode<M> Platform_data_source {get;}
--- br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource/src/1.0.0.0/IDataSource.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource/src/1.0.0.0/IDataSource.cs
@@ -1,12 +1,13 @@
 using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
 using br.ufc.mdcc.hpcshelf.platform.Maintainer;
-using br.ufc.mdcc.hpcshelf.gust.graph.InputFormat;
+//using br.ufc.mdcc.hpcshelf.gust.graph.InputFormat;
 
 namespace br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource
 {
 	public interface IDataSource<P, GIF> : BaseIDataSource<P, GIF>
 		where P:IMaintainer
-		where GIF:IInputFormat
+		where GIF:IData //InputFormat
 	{
 	}
 }
