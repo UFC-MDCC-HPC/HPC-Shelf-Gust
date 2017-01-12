fff33bddc3f34ddec5c34ded4eed0995fb52fb12
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-01-11 23:14:56 -0300

    OutputFormat

--- br.ufc.mdcc.hpcshelf.mapreduce.impl.custom.MapFunctionForward/src/1.0.0.0/IMapFunctionForward.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.impl.custom.MapFunctionForward/src/1.0.0.0/IMapFunctionForward.cs
@@ -1,16 +1,27 @@
+using System;
+using br.ufc.pargo.hpe.backend.DGAC;
+using br.ufc.pargo.hpe.basic;
 using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
+using br.ufc.mdcc.common.Iterator;
+using br.ufc.mdcc.common.KVPair;
+using br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction;
 
-namespace br.ufc.mdcc.model.Model { 
-
-public interface IModel : BaseIModel
+namespace br.ufc.mdcc.hpcshelf.mapreduce.impl.custom.MapFunctionForward
 {
-		// void loadFrom (IModel o);
-		// IModel newInstance ();
-		// IModel clone();
-
-		object Instance { get; set;}
-		object newInstance ();
-
-} // end main interface 
-
-} // end namespace 
+	public class IMapFunctionForward<IKey, IValue, TKey, TValue> : BaseIMapFunctionForward<IKey, IValue, TKey, TValue>, IMapFunction<IKey, IValue, TKey, TValue>
+where IKey:IData
+where IValue:IData
+where TKey:IData
+where TValue:IData
+	{
+		public override void main()
+		{
+			IIteratorInstance<IKVPair<TKey,TValue>> output_value_instance = (IIteratorInstance<IKVPair<TKey,TValue>>) Output_data.Instance;
+			IKVPairInstance<TKey,TValue> item = (IKVPairInstance<TKey,TValue>) Output_data.createItem();
+			item.Key = Input_key.Instance;
+			item.Value = Input_value.Instance;
+			output_value_instance.put(item);
+		}
+	}
+}
